f8e1b9a9ba70735b55515a4b213be458e6f3f963
zzy <348429898@qq.com>
2017-09-20 14:33:41 +0800

    100%

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -67,7 +67,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: 'assets/',
+  publicPath: '/assets/',
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
